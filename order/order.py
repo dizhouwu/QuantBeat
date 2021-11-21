50c3de06084cd6445b026b525a536cae91c7f9e8
BruceWGH <samwu0808@outlook.com>
2021-11-20 20:14:38 -0600

    chore: add test folder

--- order/order.py
+++ order/order.py
@@ -11,21 +11,41 @@ class Order:
         self.order_id = order_id
 
 
-class CancelOrder:
+class CancelOrder(Order):
+    __slots__ = "order_id"
+
     def __init__(self, order_id):
         super().__init__(order_id)
 
 
 class MarketOrder(Order):
+    __slots__ = "order_id", "side", "size"
+
     def __init__(self, order_id, side, size):
         super().__init__(order_id)
 
         self.side = side
         self.size = size
+        self.remaining = size
 
 
 class LimitOrder(Order):
+    __slots__ = "order_id", "side", "size", "price"
+
     def __init__(self, order_id, side, size, price):
         super().__init__(order_id)
         self.side = side
         self.price = price
+        self.size = size
+
+    def __lt__(self, other):
+        if self.price != other.price:
+            if self.side == Side.BUY:
+                return self.price > other.price
+            else:
+                return self.price < other.price
+        elif self.time != other.time:
+            return self.time < other.time
+
+        elif self.size != other.size:
+            self.size < other.size
--- order/orderbook.py
+++ order/orderbook.py
@@ -1,9 +1,14 @@
 from order import MarketOrder, LimitOrder, CancelOrder
 from sortedcontainers import SortedList
 from functools import singledispatchmethod
+from order import Side
+from trade import Trade
+from typing import Union
 
 
 class OrderBook:
+    __slots__ = "bids", "asks", "trades"
+
     def __init__(self):
         self.bids = SortedList()
         self.asks = SortedList()
@@ -15,13 +20,18 @@ class OrderBook:
 
     @process_order.register
     def _(self, order: MarketOrder):
-        ...
+        self._process_order(order)
 
-    @process_order.regester
+    @process_order.register
     def _(self, order: LimitOrder):
-        ...
+        self._process_order(order)
+        if order.remaining > 0:
+            if order.side == Side.BUY:
+                self.bids.add(order)
+            else:
+                self.asks.add(order)
 
-    @process_order.regester
+    @process_order.register
     def _(self, order: CancelOrder):
         for bid in self.bids:
             if bid.id == order.id:
@@ -31,3 +41,62 @@ class OrderBook:
             if ask.id == order.id:
                 self.asks.discard(order)
                 return
+
+    def _process_order(self, order: Union[MarketOrder, LimitOrder]):
+        if order.side == Side.BUY:
+            condition = len(self.asks) > 0
+        else:
+            condition = len(self.bids) > 0
+
+        while condition:
+            if order.side == Side.BUY:
+                book_order = self.asks.pop(0)
+            else:
+                book_order = self.bids.pop(0)
+
+            if order.remaining >= book_order.remaining:
+                order.remaining -= book_order.remaining
+                book_order.remaining = 0
+
+                self.trades.append(
+                    Trade(
+                        order.side,
+                        book_order.price,
+                        order.size,
+                        order.order_id,
+                        book_order.order_id,
+                    )
+                )
+                if order.remaining == 0:
+                    break
+            else:
+                order.remaining -= book_order.remaining
+                book_order.remaining = 0
+
+                self.trades.append(
+                    Trade(
+                        order.side,
+                        book_order.price,
+                        order.size,
+                        order.order_id,
+                        book_order.order_id,
+                    )
+                )
+
+                if order.size == Side.SELL:
+                    self.asks.add(order)
+                else:
+                    self.bids.add(order)
+
+    def get_best_bid(self):
+        return self.bids[0]
+
+    def get_best_ask(self):
+        return self.asks[0]
+
+    def __repr__(self):
+        print(f"bids: {self.bids}")
+        print(f"asks: {self.asks}")
+
+    def __len__(self):
+        return len(self.asks) + len(self.bids)
