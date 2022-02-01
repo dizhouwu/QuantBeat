b0a24f015e9cd5fdd510e2bdc7021d81401c96fc
Morty <samwu0808@outlook.com>
2022-01-31 18:44:25 -0600

    Update file_raii.cpp

--- cpplearn/raii/file_raii.cpp
+++ cpplearn/raii/file_raii.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 #include <exception>
 
-class BadFile: virtual public std::exception
+class BadFile : virtual public std::exception
 {
 public:
-    explicit BadFile(const std::string& msg):
-            m_msg(msg)
+    explicit BadFile(const std::string& msg) :
+        m_msg(msg)
     {}
 
     virtual ~BadFile() throw () {}
@@ -23,24 +23,25 @@ private:
 class FileHandlerRAII
 {
 public:
-    FileHandlerRAII(const char* fileName) : m_fp(std::fopen(fileName, "w+"))
+    FileHandlerRAII(const char* fileName) 
     {
-        if (m_fp == NULL) {
+        int ret = fopen_s(&m_fp, "test.txt", "w+");
+        if (ret != 0) {
             throw BadFile(std::string(fileName) + " failed to be opened");
         }
         std::cout << "file resource: " << fileName << " acquired\n";
     }
 
-    FileHandlerRAII(const FileHandlerRAII&)=delete; // no copy operations
-    FileHandlerRAII& operator=(const FileHandlerRAII&)=delete;
+    FileHandlerRAII(const FileHandlerRAII&) = delete; // no copy operations
+    FileHandlerRAII& operator=(const FileHandlerRAII&) = delete;
 
-    FileHandlerRAII(FileHandlerRAII&&)=delete; // no move operations
-    FileHandlerRAII& operator=(FileHandlerRAII&&)=delete;
+    FileHandlerRAII(FileHandlerRAII&&) = delete; // no move operations
+    FileHandlerRAII& operator=(FileHandlerRAII&&) = delete;
 
     bool write(const char* str)
     {
-        if( std::fputs(str, m_fp) == EOF ) {
-            return false ;
+        if (std::fputs(str, m_fp) == EOF) {
+            return false;
         }
         return true;
     }
@@ -48,28 +49,26 @@ public:
     ~FileHandlerRAII()
     {
         if (m_fp != NULL) {
-            std::fclose(m_fp) ;
+            std::fclose(m_fp);
             std::cout << "file resource released\n";
         }
     }
 private:
-    FILE *m_fp;
+    FILE* m_fp;
 };
 
 int main()
 {
     try {
-        FileHandlerRAII raii("test.log");
+        FileHandlerRAII raii("test.txt");
 
-        for (auto i = 0; i <3; i++) {
+        for (auto i = 0; i < 3; i++) {
             if (raii.write("Hello RAII")) {
                 std::cout << "file accessed successfully\n";
             }
-
-            throw BadFile("fuck this shit");
         }
     }
-    catch(...) {
+    catch (...) {
         std::cout << "something went wrong \n";
     }
 
