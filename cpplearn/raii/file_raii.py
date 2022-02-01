456514624971423daa24d2d7367e757d056b3013
Morty <samwu0808@outlook.com>
2022-01-31 18:44:36 -0600

    Update file_raii.py

--- cpplearn/raii/file_raii.py
+++ cpplearn/raii/file_raii.py
@@ -5,16 +5,21 @@ from contextlib import contextmanager
 
 @contextmanager
 def file_handler(file, mode):
-    resource = open(file, mode)
     try:
-        print("file accessed successfully")
+        print(f"file resource: {file} acquired")
+        resource = open(file, mode)
+    except OSError:
+        raise BadFile(f"{file} failed to be opened")
+    try:
         yield resource
     finally:
         print("file resource released\n")
         resource.close()
 
 try:
-    with file_handler("20220126_premarketCorpPfdLamrCorporateActionsV2.cax", "r"):
-        raise BadFile
+    with file_handler("test.txt", "w+") as f:
+        for i in range(3):
+            f.write("Hello RAII")
+            print("file accessed successfully")
 except:
     print("something went wrong")
