1eac6866a6c185c671298e8836c29e9d11e672ab
BruceWGH <samwu0808@outlook.com>
2022-02-02 20:58:51 -0600

    add flowrisk by  Xixuan

---
+++ external/flowrisk/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+#   Created by Xixuan on Nov 20, 2018
+#
+
+from flowrisk.toxicity.vpin import RecursiveVPINConfig, RecursiveVPIN
+from flowrisk.toxicity.vpin import BulkVPINConfig, BulkVPIN
+from flowrisk.toxicity.vpin import BulkConfVPINConfig, BulkConfVPIN
+
+from flowrisk.tools import vol
+from flowrisk.toxicity import vpin, bulk
+
+import flowrisk.measure as measure
+import flowrisk.examples as examples
+
+
+__name__ = "Flow Risk"
+__version__ = 0.3
+__all__ = {vol, bulk, measure, vpin, examples}
---
+++ external/flowrisk/examples/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#
+# Created by Xixuan on Nov 22, 2018
+#
+
+from flowrisk.examples.usstocks import USStocks
