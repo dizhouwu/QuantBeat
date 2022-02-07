8e3b5d221aec0d63d4a5f8c7aa0f024e70d25b0b
Morty <samwu0808@outlook.com>
2022-02-07 16:45:33 -0600

    Create linear_regression.py

---
+++ data_analysis/linear_regression.py
@@ -0,0 +1,40 @@
+import numpy as np
+from sklearn.base import BaseEstimator, RegressorMixin
+from cvxopt import matrix, solvers
+import pandas as pd
+
+class ConvexLinearModel(BaseEstimator, RegressorMixin):
+    def __init__(self, show_progress=False):
+        self.show_progress = show_progress
+
+    def fit(self, X, y):
+        n, p = X.shape
+        Xt = X.T
+        P = matrix(np.dot(Xt, X))
+        q = -matrix(np.dot(Xt, y))
+        self.coef_ = np.asarray(solvers.qp(P, q, options={
+                                "show_progress": self.show_progress})['x']).T[0]
+        return self
+
+    def predict(self, X):
+        check_is_fitted(self)
+        X = check_array(X)
+        return np.dot(X, self.coef_)
+
+    def fit_predict(self, X, y):
+        return self.fit(X, y).predict(X)
+      
+data = pd.DataFrame( {"x": np.random.normal(0,0.1,100)})
+data.insert(0, "const", np.ones(len(data)))
+data.insert(0, "Y", 2 * data['x'] + np.random.normal(0, 0.01, 100))
+
+X_cols = data.columns[1:]
+cvx_lm = ConvexLinearModel(False)
+
+y_hat = cvx_lm.fit_predict(data.iloc[:,1:], data.iloc[:,0])
+
+lr = LinearRegression(fit_intercept=True).fit(data.iloc[:,2:], data.iloc[:,0])
+y_hat2 = lr.predict(data.iloc[:,2:])
+
+
+np.allclose(y_hat, y_hat2)
